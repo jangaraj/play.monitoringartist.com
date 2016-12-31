245bbba2caacab521d9c470686540a9eb8bb66d2
Jan Garaj <jan.garaj@gmail.com>
2016-12-31 23:46:48 +0100

    Init

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ elif [ "$PAR" == "qstart" ]; then
 elif [ "$PAR" == "stop" ]; then
   #docker-compose kill
   docker rm -f $(docker ps -a | grep ' playmonitoringartistcom_play-' | awk '{print $1}')
+  docker rm -f $(docker ps -a -f status=dead | awk '{print $1}')
 else
   echo "Unknown parameter: ${PAR}"
 fi
