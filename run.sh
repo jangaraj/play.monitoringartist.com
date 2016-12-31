0888f08e384dd3947f75325d86062da420013bf4
Jan Garaj <jan.garaj@gmail.com>
2016-12-31 20:07:22 +0100

    Init

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ PAR=${1:-start}
 
 if [ "$PAR" == "start" ]; then
   docker-compose pull && docker-compose -p play-monitoringartist-com up -d --force-recreate --remove-orphans -t 1
+elif [ "$PAR" == "qstart" ]; then
+  docker-compose -p play-monitoringartist-com up -d --force-recreate --remove-orphans -t 1
 elif [ "$PAR" == "stop" ]; then
   #docker-compose kill
   docker rm -f $(docker ps -a | grep ' playmonitoringartistcom_play-' | awk '{print $1}')
